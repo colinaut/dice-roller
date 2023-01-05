e74b118a5960ac3935119fffb6479d04dbb51af6
Colin Fahrion <colin.fahrion@gmail.com>
2023-01-04 22:36:28 -0800

    feat: added dice total and max functions

--- src/main.ts
+++ src/main.ts
@@ -10,39 +10,92 @@ export default class DiceRoller extends HTMLElement {
 		return this.getAttribute(attr) || fallback;
 	}
 
-	get amount(): number {
-		return Number(this.getA("amount", "1"));
-	}
-	get sides(): number {
-		return Number(this.getA("sides", "6"));
+	private getArray(attr: string, fallback: string[] = []): string[] {
+		const temp = this.getA(attr, "");
+		if (!temp) return fallback;
+
+		let tempArray: string[] = [];
+		try {
+			// check to see if this is a JSON string
+			tempArray = JSON.parse(temp);
+		} catch (err) {
+			// check to see if this a comma separated string
+			tempArray = temp.split(",");
+		}
+		if (Array.isArray(tempArray)) {
+			return tempArray;
+		} else return fallback;
 	}
 
-	get roll(): number {
-		return Number(this.getA("roll", this.sides.toString()));
+	get dice(): string[] {
+		const diceAttr = this.getAttribute("dice") || "6";
+		const diceArray = diceAttr.split(",");
+		return diceArray;
 	}
 
-	set roll(roll) {
-		this.setAttribute("roll", roll.toString());
+	get modifier(): number {
+		const modifier = this.getAttribute("modifier") || 0;
+		return Number(modifier);
+	}
+
+	get bestOf(): number {
+		const bestOf = this.getAttribute("bestOf") || this.dice.length;
+		return Number(bestOf);
 	}
 
 	static get observedAttributes(): string[] {
-		return ["amount", "sides", "roll"];
+		return ["dice", "modifier", "bestOf"];
 	}
 
+	private dieRolls: number[] = [];
+	private total: number = 0;
+
 	public connectedCallback(): void {
+		this.rollDice();
 		this.render();
 		this.addEventListeners();
+	}
+
+	private rollDie(sides: number) {
+		return Math.floor(Math.random() * sides + 1);
+	}
+
+	private rollDice() {
+		this.dice.forEach((die, i) => {
+			this.dieRolls[i] = this.rollDie(Number(die));
+		});
+		this.total =
+			this.dieRolls.reduce(function (accum, curValue) {
+				return accum + curValue;
+			}, 0) + this.modifier;
+	}
+
+	private maxDie(): number {
+		const max: number = this.dieRolls.reduce(function (a: number, b: number) {
+			return Math.max(a, b);
+		}, -Infinity);
+		return max;
+	}
+
+	private twoHighest(): number[] {
+		return this.dieRolls.reduce(
+			(acc, rec) => {
+				return rec > acc[1] ? [acc[1], rec] : rec > acc[0] ? [rec, acc[1]] : acc;
+			},
+			[0, 0]
+		);
 	}
 
 	private addEventListeners(): void {
 		this.shadow.addEventListener(
 			"click",
-			(event) => {
+			() => {
 				// console.log(event);
 
-				const changeDieFace = (roll: number) => {
-					roll = Math.floor(Math.random() * this.sides + 1);
-					this.roll = roll;
+				const changeDieFace = () => {
+					// console.log("🚀 ~ file: main.ts:49 ~ DiceRoller ~ changeDieFace ~ this.dieRoll", this.dieRoll);
+					this.rollDice();
+					this.render();
 				};
 
 				let nIntervalId: number | undefined;
@@ -60,15 +113,21 @@ export default class DiceRoller extends HTMLElement {
 	}
 
 	public attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-		// console.log("changed", name, oldValue, newValue);
+		console.log("changed", name, oldValue, newValue);
 
 		this.render();
 	}
 
 	private render() {
+		const modifier: string = this.modifier > 0 ? "+" + this.modifier.toString() : this.modifier < 0 ? this.modifier.toString() : "";
 		const css = `
         <style>
             .dice {
+                display: flex;
+                gap: .8rem;
+                flex-wrap: wrap;
+            }
+            .die {
                 width: 5rem;
                 height: 5rem;
                 border: 2px solid #fff;
@@ -76,18 +135,39 @@ export default class DiceRoller extends HTMLElement {
                 display: flex;
                 align-items: center;
                 justify-content: center;
-            }
-            .num {
                 font-size: 3rem;
+            }
+            .total {
+                height: 5rem;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 3rem;
+                order: 2px solid transparent;
+                font-weight: bold;
+                padding-left: .5rem;
+            }
+            .highest {
+                height: 5rem;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 1rem;
+                order: 2px solid transparent;
+                font-weight: bold;
+                padding-left: .5rem;
             }
         </style>
         `;
 
 		let html = `
         <div>
-            <h3>${this.amount}d${this.sides}</h3>
-            <div class="dice" >
-               <div class="num">${this.roll}</div>
+            <h3>${this.dice} <span>${modifier}</span></h3>
+            <div class="dice">
+                ${this.dieRolls.map((roll) => `<div class="die">${roll}</div>`).join("")} \
+                <div class="total">${this.total}</div>
+                <div class="highest">Highest: ${this.maxDie()}</div>
+                <div class="highest">Two Highest: ${this.twoHighest()}</div>
             </div>
         </div>
         `;
